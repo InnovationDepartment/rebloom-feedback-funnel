5e2937a297951b44191e381f2c0242bb3d32cca1
png262 <png262@gmail.com>
2019-06-14 15:19:21 -0400

    prettier width 100

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,19 +1,12 @@
 module.exports = {
-  printWidth: 80, // 80
+  printWidth: 100, // 100
   tabWidth: 2, // 2
   useTabs: false, // false
   semi: false, // true
   singleQuote: true, // false
   trailingComma: 'es5', // none | es5 | all
   bracketSpacing: true, // true
-  overrides: [
-    {
-      files: '*src/app.js',
-      options: {
-        printWidth: 100,
-      },
-    },
-  ],
+  overrides: [],
   // jsxBracketSameLine: false // false
   // arrowParens: avoid // avoid | always
 }
